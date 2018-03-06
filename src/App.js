09a61d53474d71723bb11e5115d6bd0bc9703183
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-05 18:41:01 -0600

    Broken ClassList?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ClassList from './components/ClassList';
 
 const App = () => (
   <div>
     <h2>Todo App</h2>
+    <ClassList />
 
   </div>
 );
--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -1,14 +1,39 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
 
 class ClassList extends Component {
     constructor() {
         super();
 
         this.state = {
-            listItem: [],
-            newItem: ''
+            listNames: [],
+            newListNames: ''
         };
     }
 
-    
+    newClassList = (event) => {
+        event.preventDefault();
+        const namesList = this.state.listNames;
+        namesList.push(this.state.newListNames);
+        this.setState({
+            newListNames: '',
+            names: namesList
+        });
+    };
+
+    handleNewName = (event) => {
+        this.setState({ newList: event.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.names.map(name => <div>{name}</div>)}
+                <form onSubmit={this.newClassList}>
+                    <input type='text' onChange={this.handleNewName} placeholder="Add a new item" value={this.state.newListName} />
+                </form>
+            </div>
+        );
+    }
 }
+
+export default ClassList;
